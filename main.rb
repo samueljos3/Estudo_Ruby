d3a8a7a07a7b8432cc3a7761ee4d7358815b4f7c
Samuel José <131408841+samueljos3@users.noreply.github.com>
2023-07-02 15:53:15 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,71 @@
+=begin
+puts "Hello World!"
+
+# tipos de dados:
+
+num = 10
+puts num.class
+
+palavra = "coisa"
+puts palavra.class 
+
+num_f = 10.5
+puts num_f.class
+
+bool1 = true
+puts bool1.class
+
+bool2 = false
+puts bool2.class
+
+array_qualquer = [2, 3, 5, 4]
+puts array_qualquer.class
+puts array_qualquer[1]
+
+symbol_qualquer = :simbolo  # usado para representar alguma coisa
+puts symbol_qualquer
+
+hash_qualquer = {nome: 'Luiz', idade: '44'}
+puts hash_qualquer
+puts hash_qualquer[:nome]
+
+# input
+
+puts "Digite o seu nome: "
+name = gets.chomp
+puts "Hello #{name}!"
+
+puts "Digite um número inteiro: "
+int1 = gets.chomp.to_i
+puts "Digite outro número inteiro: "
+int2 = gets.chomp.to_i
+puts "A soma dos números é #{int1 + int2}"
+
+# estruturas de controle:
+
+puts "Digite o dia da semana: "
+dia = gets.chomp
+
+if dia == "sábado" or dia == "domingo"
+    tipo_dia = "fim de semana"
+elsif dia == "segunda"
+  tipo_dia = "o início das aulas"
+else 
+  tipo_dia = "um dia Qualquer"
+end
+
+puts "Hoje é #{tipo_dia}"
+=end
+
+puts "Digite o dia da semana em inteiro: "
+dia_semana = gets.chomp.to_i
+
+case dia_semana
+
+when 2..5
+  puts "Não é fim de semana!"
+when 6, 7
+  puts "É fim de semana!"
+else
+  puts "Valor inválido"
+end
